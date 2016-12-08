57e893572b364316ec5845a290d274cec3331202
Andrew Ellis <taellis17@transy.edu>
2016-12-08 11:17:16 -0500

    global.h

---
+++ global.h
@@ -0,0 +1,13 @@
+#ifndef GLOBAL
+#define GLOBAL
+	using namespace std;
+
+	unsigned long int GCOUNT;
+	ofstream globalOutputFile;
+	ifstream globalInputFile;
+	unsigned char noteArray[MAXARRAYSIZE];
+	unsigned int lengthArray;
+	unsigned char division;
+	unsigned int numTracks;
+	MeasureList measureList;
+#endif
